673d00462c8ad4c2b6c31f8499790d7bf50c10af
sealfury <willspecker@gmail.com>
2021-12-07 17:04:21 +0100

    chunk solutions - simple and elegant (smooth)

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,46 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// SOLUTION 1
+const chunkSimple = (array, size) => {
+  // Create empty array to hold chunks called 'chunked'
+  const chunked = []
+  // For each element in 'unchunked' array
+  for (let element of array) {
+    // Retrieve last element in 'chunked'
+    const last = chunked[chunked.length - 1]
 
-module.exports = chunk;
+    // IF last element does not exist or if length is eq to chunk size
+    if (!last || last.length === size) {
+      // Push a new chunk into 'chunked' w/ current element
+      chunked.push([element])
+    } else {
+      // ELSE add current element into the chunk
+      last.push(element)
+    }
+  }
+
+  return chunked
+}
+
+// SOLUTION 2
+const chunkSmooth = (array, size) => {
+  // Create empty 'chunked' array
+  const chunked = []
+  // Create 'index'; start at 0
+  let index = 0
+
+  // While index is less than array.length
+  while (index < array.length) {
+    // Push a slice of length 'size' from 'array' into 'chunked'
+    chunked.push(array.slice(index, index + size))
+
+    // Add 'size' to 'index'
+    index += size
+  }
+
+  return chunked
+}
+
+module.exports = chunkSmooth
+module.exports = chunkSimple
