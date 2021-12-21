5c2b82251b47c662210068cd93257e21bbd49e02
sealfury <willspecker@gmail.com>
2021-12-21 12:38:43 +0100

    implements LL size-calculating method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,11 +13,23 @@ class LinkedList {
   constructor() {
     this.head = null
   }
-  
+
   insertFirst(data) {
     this.head = new Node(data, this.head)
     // const node = new Node(data, this.head)
     // this.head = node
+  }
+
+  size() {
+    let counter = 0
+    let node = this.head
+
+    while (node) {
+      counter++
+      node = node.next
+    }
+
+    return counter
   }
 }
 
