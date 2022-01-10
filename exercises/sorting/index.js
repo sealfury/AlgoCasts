f947e40a70e8a75b729062c01e7cbd44cbb631cd
sealfury <willspecker@gmail.com>
2022-01-10 19:20:35 +0100

    selection sort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -19,7 +19,25 @@ const bubbleSort = arr => {
 
 //  Worst-case runtime: n^2 (fine for F.E. apps with e.g. 100 records)
 //  Method Difficulty: easier
-function selectionSort(arr) {}
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j
+      }
+    }
+
+    if (indexOfMin !== i) {
+      let lesserVal = arr[indexOfMin]
+      arr[indexOfMin] = arr[i]
+      arr[i] = lesserVal
+    }
+  }
+
+  return arr
+}
 
 //  Worst case runtime: n*log(n)
 //  Method Difficulty: medium
