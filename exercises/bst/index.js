1a37629aada71b552b3028fa7807f5aa8bcdd4df
sealfury <willspecker@gmail.com>
2022-01-10 15:43:11 +0100

    BTS - contains method (recursive search) solution

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -29,6 +29,21 @@ class Node {
       this.right = new Node(data)
     }
   }
+
+  // contains - essentially a recursive search through the tree
+  contains(data) {
+    if (this.data === data) {
+      return this
+    }
+
+    if (this.data < data && this.right) {
+      return this.right.contains(data)
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data)
+    }
+
+    return null
+  }
 }
 
 module.exports = Node
