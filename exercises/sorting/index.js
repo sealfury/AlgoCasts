e6cc91df9d94630058f29bc44c06db12d16678ba
sealfury <willspecker@gmail.com>
2022-01-11 21:23:17 +0100

    merge fn sort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -43,6 +43,18 @@ const selectionSort = arr => {
 //  Method Difficulty: medium
 function mergeSort(arr) {}
 
-function merge(left, right) {}
+const merge = (left, right) => {
+  const results = []
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+  }
+
+  return [...results, ...left, ...right]
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge }
